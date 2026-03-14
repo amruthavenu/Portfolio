717632a41305d3370660fb2bd60c06dfd94bd6fe
amruthavenu <amruthavenuvt@gmail.com>
2026-03-14 20:38:00 +0100

    feat: initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,46 @@
 import Image from "next/image";
+import PortfolioPage from "./portfolio";
 
 export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
-  );
+  return  <PortfolioPage />;
+    /* (
+  <main className="p-10 max-w-4xl mx-auto">
+
+      <h1 className="text-4xl font-bold mb-4">
+        Amrutha Venugopal
+      </h1>
+
+      <p className="text-lg mb-6">
+        Backend Developer | Java | Spring Boot | Django
+      </p>
+
+      <section className="mb-10">
+        <h2 className="text-2xl font-semibold mb-3">About Me</h2>
+        <p>
+         Backend Software Engineer with 4+ years of experience building scalable backend systems and enterprise applications. Experienced in Java (Spring Boot), Python (Django), RESTful APIs, and microservices architectures with PostgreSQL and Oracle databases. Proven experience delivering production systems in automotive environments at Daimler Truck and developing distributed backend services in Agile teams. Strong focus on clean architecture, test-driven development, and cloud-based deployment using Azure and Docker
+        </p>
+      </section>
+
+      <section className="mb-10">
+        <h2 className="text-2xl font-semibold mb-3">Projects</h2>
+
+        <ul className="space-y-3">
+          <li>
+            Fleet Insight Manager — Vehicle reservation platform built with .NET and Angular.
+          </li>
+          <li>
+            Vehicle Dock Management — Django web application for vehicle area management.
+          </li>
+        </ul>
+      </section>
+
+      <section>
+        <h2 className="text-2xl font-semibold mb-3">Contact</h2>
+        <p>Email: amrutha@email.com</p>
+        <p>LinkedIn: linkedin.com/in/amrutha</p>
+      </section>
+
+    </main> 
+   
+  )*/
 }
